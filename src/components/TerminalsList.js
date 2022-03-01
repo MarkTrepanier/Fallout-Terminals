a0221b7fcae5b95385197e024ea342e7ec7f2203
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-01 14:50:48 -0500

    adds todos for tomorrows work

--- src/components/TerminalsList.js
+++ src/components/TerminalsList.js
@@ -6,3 +6,8 @@ export default function TerminalsList(props) {
       {data.map((terminal)=><div>{terminal.terminal_name}</div>)}
    </div>
 };
+//todo: deal with react plugin bug
+//todo: add brackets to terminal names
+//todo: make text unselectable
+//todo: make clickable, returning page data?
+//todo: switch from dummy data to backend support
