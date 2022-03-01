243d9366e336f673faf21c90cb34e10e3f3d2541
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-01 14:46:41 -0500

    adds terminal names dummy data. pulled into TerminalList

---
+++ src/DummyData/terminals.js
@@ -0,0 +1,20 @@
+let terminals = [
+    {
+        terminal_name: "2222022211: RobCo: VisRep0"
+    },
+    {
+        terminal_name: "2222022213:RobCo:VisRep1"
+    },
+    {
+        terminal_name: "2222022214:RobCo:VisRep2"
+    },
+    {
+        terminal_name: "2222022215:RobCo:VisRep3"
+    },
+    {
+        terminal_name: "2222022216:RobCo:VisRep4"
+    }
+]
+export default function terminalsList(){
+    return terminals
+}
--- src/components/TerminalsList.js
+++ src/components/TerminalsList.js
@@ -1,9 +1,8 @@
+import terminalsData from "../DummyData/terminals"
+
+let data = terminalsData()
 export default function TerminalsList(props) {
    return <div>
-      <div>[2222022211:RobCo:VisRep0]</div>
-      <div>[2222022213:RobCo:VisRep1]</div>
-      <div>[2222022214:RobCo:VisRep2]</div>
-      <div>[2222022215:RobCo:VisRep3]</div>
-      <div>[2222022216:RobCo:VisRep4]</div>
+      {data.map((terminal)=><div>{terminal.terminal_name}</div>)}
    </div>
 };
