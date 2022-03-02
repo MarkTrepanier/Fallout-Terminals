e6954cdd755247eb8329a5331f81e8c86b97bf02
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-02 17:05:36 -0500

    makes terminal name text unselectable

--- src/components/TerminalsList.js
+++ src/components/TerminalsList.js
@@ -1,12 +1,20 @@
 import terminalsData from "../DummyData/terminals"
+import Styled from  "styled-components"
 
 let data = terminalsData()
 export default function TerminalsList(props) {
    return <div>
-      {data.map((terminal)=><div key={terminal.terminal_name} onClick={()=>console.log(terminal.terminal_name)}>[{terminal.terminal_name}]</div>)}
+      {data.map((terminal) => <TerminalNameStyle key={terminal.terminal_name} onClick={() => console.log(terminal.terminal_name)}>[{terminal.terminal_name}]</TerminalNameStyle>)}
    </div>
 };
-//todo: deal with react plugin bug
+
+const TerminalNameStyle = Styled.div`
+   -webkit-user-select: none; /* Safari */        
+   -moz-user-select: none; /* Firefox */
+   -ms-user-select: none; /* IE10+/Edge */
+   user-select: none; /* Standard */
+`
+
 //todo: make text unselectable
 //todo: make clickable, returning page data?
 //todo: switch from dummy data to backend support
