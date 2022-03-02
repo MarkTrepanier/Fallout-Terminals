a47396a77f899d8c975015d74fce2dcd7409de03
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-03-02 16:47:01 -0500

    makes terminal names clickable. returns terminal name

--- src/components/TerminalsList.js
+++ src/components/TerminalsList.js
@@ -3,7 +3,7 @@ import terminalsData from "../DummyData/terminals"
 let data = terminalsData()
 export default function TerminalsList(props) {
    return <div>
-      {data.map((terminal)=><div>[{terminal.terminal_name}]</div>)}
+      {data.map((terminal)=><div key={terminal.terminal_name} onClick={()=>console.log(terminal.terminal_name)}>[{terminal.terminal_name}]</div>)}
    </div>
 };
 //todo: deal with react plugin bug
