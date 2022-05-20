2720114db1c59f3f77ffa3bec92327dc0909a544
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-05-20 15:09:36 -0400

    include sound. imprts to monitor

--- src/components/monitor.js
+++ src/components/monitor.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 import Styled from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import ActionRoller from "./ActionRoller";
-//import Login from "./login";
+import useSound from "use-sound";
 import TerminalsList from "./TerminalsList";
 import Terminal from "./Terminal";
+import electricHum from '../assets/sounds/hum_electric_neon_light_01.wav'
 
 export default function Monitor(props) {
   const[activeTerminal, setActiveTerminal]=useState("")
